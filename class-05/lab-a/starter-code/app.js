73b07df587f0c961e41bc40dc56fcadbdb3cadd1
mustafahamdanah <mustafahamdan1994@gmail.com>
2020-02-21 18:40:35 +0200

    add q-four

--- class-05/lab-a/starter-code/app.js
+++ class-05/lab-a/starter-code/app.js
@@ -79,12 +79,19 @@ Test this function by hand in the console to get it working, and when you think 
 var testArray = [2, 3, 4]; //eslint-disable-line
 
 function sumArray(sumArr) { //eslint-disable-line
-
+    var sumArray1 = 0;
+for (var i=0;i <sumArr.length;i++){
+    sumArray1 = sum(sumArray1,sumArr[i])[0];
 }
+var message3 = `${2},${3},${4} was passed in as an array of numbers, and ${9} is their sum.`;
+return [sumArray1,message3];
+}
+//sumArray(testArray);
+
 
 // Here is the test for sumArray(); uncomment it to run it
 
-// testSumArray(testArray);
+testSumArray(testArray);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
